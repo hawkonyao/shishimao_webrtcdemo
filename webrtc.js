ed6c0ddbfbc88c54d337da523adb1dbe836d4107
hawkon <hawkon@126.com>
2016-03-10 22:25:50 +0800

    优化代码，实现 1、屏幕和摄像头分享 2、切换大小屏。 3、切换播放人时自动关闭数据通道。 4、播放人手动关闭/开启屏幕或摄像头。
    Signed-off-by: hawkon <hawkon@126.com>

--- webrtc.js
+++ webrtc.js
@@ -5,13 +5,11 @@ app.service('Users', function ($http) {
         {
             name: "user6",
             token: "0cfb1079-0913-4dfd-a614-ea551556467d",
-            screen_token: "5f17e823-48c6-4e0f-9b52-a11b74e39be0",
             online: false
         },
         {
             name: "user5",
             token: "02513965-3b10-406d-a037-d0c56521d5d3",
-            screen_token: "2753f3c1-569a-42e1-8f09-91592ed6da3b",
             online: false
         },
         {name: "user4", token: "c95b5788-fdbd-4193-861a-87c914cfe716", online: false},
@@ -48,10 +46,13 @@ app.controller("rtcCtl", function ($scope, $http, $location, Users) {
     $scope.users = Users.getList();
     $scope.log = "";
     $scope.currentuser = Users.getUser($location.path().replace("/", ""));
-    $scope.addlog = function (msg) {
-
-        $scope.log += "\r\n" + msg;
+    $scope.freecontainerid="main";
+    var session = $scope.session = new RTCat.Session($scope.currentuser.token);
+    $scope.online = function () {
+        console.log('online');
+        session.connect();
     }
+
     $scope.setonline = function (user) {
         if (user == null)return;
         $scope.addlog(user.name + "上线");
@@ -63,126 +64,96 @@ app.controller("rtcCtl", function ($scope, $http, $location, Users) {
         user.online = false;
         $scope.$applyAsync(user.online);
     };
-    var session = $scope.session = new RTCat.Session($scope.currentuser.token);
-
-    session.connect();
-    $scope.shareScreen = function () {
-        RTCat.extensionId = "kopddpjmdlllnpkpcphndjiaohbakkjb";
-        $scope.initStream({screen: true, ratio: 1.33,type:"screen"}, $scope.displayScreen);
-    };
-    $scope.shareCam = function () {
-        $scope.initStream({video: true, audio: true, data: true,type:"video"}, $scope.display);
-    };
-    session.on('connected', function (users) {
-        console.log('Session connected');
-        for (var i = 0; i < users.length; i++) {
-            $scope.setonline(Users.getUserByToken(users[i]));
-        }
-        $scope.setonline($scope.currentuser);
-
-    });
-
-    session.on('in', function (token) {
-        var user = Users.getUserByToken(token);
-        $scope.setonline(user);
-        console.log('someone in');
-        if ($scope.playinguser) {
-            $scope.session.sendMessage($scope.currentuser.token, JSON.stringify({playing: $scope.playinguser.token}))
-        }
-    });
-
-    session.on('out', function (token) {
-        var user = Users.getUserByToken(token);
-        $scope.setoffline(user);
-        console.log('someone out');
-    });
-
-    setInterval(function () {
-        $scope.$applyAsync($scope.users);
-    }, 1000);
-    session.on("local", function (sender) {
-        console.log('local');
-        var user = Users.getUserByToken(sender.getReceiver());
-        user.receive = sender;
-    });
-    session.on('remote', function (r_channel) {
-        console.log('remote');
-        var id = r_channel.getId();
-
-        var user = Users.getUserByToken(r_channel.getSender());
-        user.receive = r_channel;
-        user.receive.on('detect_net', function (netState) {
-            if (netState.received > 0)
-                user.netState = {
-                    recieved: netState.received,
-                    sent: netState.sent,
-                    rtt: netState.rtt,
-                    name: user.name,
-                    token: user.token
-                };
-            //console.log(user.netState);
-
-        })
-        r_channel.on('stream', function (stream) {
-            console.log('stream');
-            var user = Users.getUserByToken(r_channel.getSender());
-
-            user.stream = stream;
-            user.hasVideo = stream.hasVideo();
-            if (user == $scope.playinguser)
-                displayStream(user.stream);
-            $scope.$applyAsync(user.hasVideo);
-        });
-        r_channel.on('close', function () {
-            console.log('close');
-            var user = Users.getUserByToken(r_channel.getSender());
-            user.stream = null;
-        });
-    });
-    session.on("message", function (token, message) {
-        var msg = JSON.parse(message);
-        $scope.addlog(message);
-        if (msg.playing) {
-            var usr = Users.getUserByToken(msg.playing);
-            if (usr == $scope.currentuser)
-                $scope.initStream({video: true, audio: true, data: true}, $scope.display);
-            $scope.playUser(usr);
-        }
-        if (msg.chat) {
-            var usr = Users.getUserByToken(token);
-            $scope.addlog(usr.name + ":" + msg.msg);
-        }
-    });
-    $scope.brodcastuser = function (user) {
-        if ($scope.playinguser == user)return;
-        $scope.session.sendMessage($scope.currentuser.token, JSON.stringify({playing: user.token}));
-        $scope.playUser(user);
+    $scope.addlog = function (msg) {
+        $scope.log += "\r\n" + msg;
     }
-    $scope.playUser = function (user) {
+    $scope.brodcastuser = function (user) {
+        console.log('brodcastuser' + user.name);
         if ($scope.playinguser == user)return;
-        if ($scope.playinguser && $scope.playinguser.stream)
-            stopStream($scope.playinguser.stream);
-        if (user == $scope.currentuser)
-            stopStream($scope.currentuser.stream);
-        else {
-            $scope.display($scope.currentuser.stream);
-        }
-        displayStream(user.stream);
         $scope.playinguser = user;
+        $scope.session.sendMessage($scope.currentuser.token, JSON.stringify({playing: user.token}));
         $scope.$applyAsync($scope.playinguser);
         $scope.addlog("主屏切换到" + user.name);
     }
-    $scope.sendChatMsg = function () {
-        $scope.session.sendMessage($scope.currentuser.token, JSON.stringify({chat: true, msg: $scope.chatmsg}));
-        $scope.addlog("我:" + $scope.chatmsg);
+    $scope.shareCam = function () {
+        $scope.stopCam();
+        $scope.initStream({video: true, audio: true, data: true, attr: {type: 'video',containerId:$scope.freecontainerid}}, $scope.play);
+    }
+    $scope.stopCam = function () {
+        $scope.closeVideo();
+    }
+    $scope.shareScreen = function () {
+        $scope.stopScreen();
+        RTCat.extensionId = "kopddpjmdlllnpkpcphndjiaohbakkjb";
+        $scope.initStream({screen: true, data: true, attr: {type: 'screen',containerId:$scope.freecontainerid}}, $scope.play);
+    }
+    $scope.stopScreen = function () {
+        $scope.closeScreen();
+    }
+    $scope.changePlay = function () {
+        $scope.currentuser.videoStream.stop();
+        $scope.currentuser.screenStream.stop();
+        $scope.play($scope.currentuser.videoStream,  $scope.currentuser.videoStream.containerid == "main" ? "second" : "main");
+        $scope.play($scope.currentuser.screenStream,  $scope.currentuser.screenStream.containerid == "main" ? "second" : "main");
+    }
+    $scope.change = function () {
+        $scope.session.sendMessage($scope.currentuser.token, JSON.stringify({change: true}));
+        $scope.changePlay();
+    }
+    $scope.play = function (stream, containerid) {
+        if (stream) {
+            stream.play(containerid);
+            stream.containerid = containerid;
+            console.log('stream.play("' + containerid + '")');
+        }
+    }
+    $scope.stop = function (stream) {
+        if (stream) {
+            stream.stop();
+            console.log('stream.stop()');
+            $scope.freecontainerid=stream.containerid;
+        }
+    }
+    $scope.stopAll=function(){
+        $scope.closeScreen();
+        $scope.closeVideo();
+        $scope.playinguser=null;
+        $scope.freecontainerid="main";
+    }
+    $scope.closeScreen=function(){
+        if (!$scope.currentuser.screensender)
+            return;
+        for (var i = 0; i < $scope.currentuser.screensender.length; i++) {
+            $scope.currentuser.screensender[i].close();
+        }
+        $scope.currentuser.screensender = [];
+        $scope.stop($scope.currentuser.screenStream);
+        $scope.currentuser.screenStream=null;
+    }
+    $scope.closeVideo=function(){
+        if (!$scope.currentuser.videosender)
+            return;
+        for (var i = 0; i < $scope.currentuser.videosender.length; i++) {
+            $scope.currentuser.videosender[i].close();
+        }
+        $scope.currentuser.videosender = [];
+        $scope.stop($scope.currentuser.videoStream);
+        $scope.currentuser.videoStream=null;
+    }
+    $scope.stopShare = function () {
+        $scope.stopAll();
+        $scope.session.sendMessage($scope.currentuser.token, JSON.stringify({stopShare: true}));
+    }
+    $scope.toggleVideo = function () {
+        $scope.currentuser.videoStream.toggleVideo();
+        $scope.currentuser.screenStream.toggleVideo();
     }
     $scope.initStream = function (options, callback) {
-
-        console.log('initStream');
-        localStream = new RTCat.Stream(options);
+        $scope.freecontainerid=options.attr.containerId=="main"?"second":"main";
+        var localStream = new RTCat.Stream(options);
         localStream.on('access-accepted', function () {
-                session.send({stream: localStream, data: true,attr:options.type});
-                callback(localStream);
+                session.send({stream: localStream, data: true, attr: options.attr});
+                callback(localStream, options.attr.containerId);
             }
         );
         localStream.on('access-failed', function (err) {
@@ -193,35 +164,105 @@ app.controller("rtcCtl", function ($scope, $http, $location, Users) {
             console.log(err);
         });
         localStream.init();
-
-    }
-    // 显示流
-    function displayStream(stream) {
-        if (stream)
-            stream.play("media");
+        if (options.attr.type == "video")
+            $scope.currentuser.videoStream = localStream;
+        if (options.attr.type == "screen")
+            $scope.currentuser.screenStream = localStream;
     }
 
-    function stopStream(stream) {
-        if (stream)
-            stream.stop();
-    }
 
-    // 显示流
-    $scope.display = function (stream) {
-        if (!stream)
-            return;
-        stream.play("media");
-        $scope.currentuser.stream = stream;
-        $scope.currentuser.hasVideo = stream.hasVideo();
-        $scope.$applyAsync($scope.currentuser.hasVideo);
-    }
-    $scope.displayScreen=function(stream){
+    session.on('connected', function (users) {
+        console.log('Session connected');
+        //获取所有在线用户的token
+        for (var i = 0; i < users.length; i++) {
+            $scope.setonline(Users.getUserByToken(users[i]));
+        }
+        $scope.setonline($scope.currentuser);
+    });
+    session.on('in', function (token) {
+        console.log('someone in');
+        var user = Users.getUserByToken(token);
+        $scope.setonline(user);
+    });
 
-        if (!stream)
-            return;
-        stream.play("screen");
-        $scope.currentuser.screenstream = stream;
-        $scope.currentuser.hasVideo = stream.hasVideo();
-        $scope.$applyAsync($scope.currentuser.hasVideo);
-    }
+    session.on('out', function (token) {
+        console.log('someone out');
+        var user = Users.getUserByToken(token);
+        $scope.setoffline(user);
+    });
+    session.on("local", function (sender) {
+        console.log('local');
+        //var user = Users.getUserByToken(sender.getReceiver());
+        if(sender.attr.type=="video") {
+            if (!$scope.currentuser.videosender)
+                $scope.currentuser.videosender = [];
+            $scope.currentuser.videosender.push(sender);
+        }
+        if(sender.attr.type=="screen") {
+            if (!$scope.currentuser.screensender)
+                $scope.currentuser.screensender = [];
+            $scope.currentuser.screensender.push(sender);
+        }
+    });
+
+    session.on('remote', function (r_channel) {
+        console.log('remote');
+        var id = r_channel.getId();
+        var user = Users.getUserByToken(r_channel.getSender());
+        user.receive = r_channel;
+        r_channel.on('detect_net', function (netState) {
+            if (netState.received > 0)
+                user.netState = {
+                    recieved: netState.received,
+                    sent: netState.sent,
+                    rtt: netState.rtt,
+                    name: user.name,
+                    token: user.token
+                };
+        });
+        r_channel.on('stream', function (stream) {
+            console.log('r_channel.stream');
+            var user = Users.getUserByToken(r_channel.getSender());
+            $scope.play(stream, r_channel.attr.containerId);
+            if (r_channel.attr.type == "video")
+                $scope.currentuser.videoStream = stream;
+            if (r_channel.attr.type == "screen")
+                $scope.currentuser.screenStream = stream;
+        });
+        r_channel.on('close', function () {
+            console.log('r_channel.close');
+            var user = Users.getUserByToken(r_channel.getSender());
+        });
+    });
+
+    session.on("message", function (token, message) {
+        console.log('message:' + message);
+        var msg = JSON.parse(message);
+        if (msg.playing) {
+            var user = Users.getUserByToken(msg.playing);
+            $scope.addlog("主屏切换到" + user.name);
+            if (user == $scope.currentuser)
+                $scope.shareCam();
+            else if($scope.playinguser==$scope.currentuser) {
+                $scope.stopAll();
+            }
+            $scope.playinguser = user;
+            $scope.$applyAsync($scope.playinguser);
+        }
+        if (msg.chat) {
+            var usr = Users.getUserByToken(token);
+            $scope.addlog(usr.name + ":" + msg.msg);
+        }
+        if (msg.change) {
+            $scope.changePlay();
+        }
+        if (msg.stopShare) {
+            $scope.playinguser=null;
+        }
+    });
+    $scope.online();
+
+    setInterval(function () {
+        $scope.$applyAsync($scope.users);
+    }, 1000);
 });
